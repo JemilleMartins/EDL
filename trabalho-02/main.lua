3b7ff68e7fdfbfb3e57378e48f82abbd9e96b504
JemilleMartins <jemilee.cristina@gmail.com>
2016-09-04 17:07:30 -0300

    Add files via upload

---
+++ trabalho-02/main.lua
@@ -0,0 +1,12 @@
+function love.load()
+   fogos = love.graphics.newImage("fogos.png")
+   
+end
+ 
+function love.draw()
+
+   love.graphics.draw(fogos, 400, 10)
+   love.graphics.setColor(0, 255, 0, 255)
+   love.graphics.print("Olá Mundo!", 200, 200)
+   
+end
